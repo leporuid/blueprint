bca861d55c6599bb32edf64303c3a41068e68233
Yu-Min Peng <leporuid@icloud.com>
2026-04-06 23:40:28 +0800

    Update flake.nix

--- templates/red-tape/flake.nix
+++ templates/red-tape/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Red-tape template using blueprint's mkFlake";
+  description = "description = "Red-tape template with Blueprint compatibility";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
