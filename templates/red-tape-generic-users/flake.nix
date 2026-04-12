071230ac44d58ee4a8731170773a37cd5627b6ac
Yu-Min Peng <leporuid@icloud.com>
2026-04-13 01:21:16 +0800

    Update flake.nix
    Signed-off-by: Yu-Min Peng <leporuid@icloud.com>

--- templates/red-tape-generic-users/flake.nix
+++ templates/red-tape-generic-users/flake.nix
@@ -1,22 +1,22 @@
 {
-  description = "blueprint + generic-users: manage NixOS/Darwin hosts with per-host home-manager users";
+  description = "Red-tape template using blueprint's mkFlake";
 
-  # Add all your dependencies here
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
 
-    # leporuid/blueprint includes the generic-users feature which auto-discovers
-    # and wires per-host users defined under hosts/<hostname>/users/<username>/
-    blueprint.url = "github:leporuid/blueprint";
-    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+    blueprint = {
+      url = "github:numtide/blueprint";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-darwin.url = "github:nix-darwin/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    red-tape = {
+      url = "github:phaer/red-tape";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  # Load the blueprint
-  outputs = inputs: inputs.blueprint { inherit inputs; };
+  outputs = inputs: (inputs.blueprint { inherit inputs; src = ./.; }) // {
+    # Re-export red-tape's mkFlake for direct access
+    inherit (inputs.red-tape) mkFlake;
+  };
 }
