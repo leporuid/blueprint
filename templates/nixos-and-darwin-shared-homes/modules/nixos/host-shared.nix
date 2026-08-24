0d162d6e051ca2f0e199c91c1fb4db78a8b72d19
Yu-Min Peng <98006840+leporuid@users.noreply.github.com>
2026-08-24 12:45:41 +0800

    Update host-shared.nix
    Signed-off-by: Yu-Min Peng <98006840+leporuid@users.noreply.github.com>

--- templates/nixos-and-darwin-shared-homes/modules/nixos/host-shared.nix
+++ templates/nixos-and-darwin-shared-homes/modules/nixos/host-shared.nix
@@ -6,5 +6,5 @@
   # you can check if host is darwin by using pkgs.stdenv.isDarwin
   environment.systemPackages = [
     pkgs.btop
-  ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.xbar ]);
+  ] ++ (pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [ pkgs.xbar ]);
 }
