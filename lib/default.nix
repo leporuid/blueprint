0aaff7e479dc677282f70527ba1329613f11f002
Yu-Min Peng <98006840+leporuid@users.noreply.github.com>
2026-08-24 11:59:51 +0800

    Update default.nix
    Signed-off-by: Yu-Min Peng <98006840+leporuid@users.noreply.github.com>

--- lib/default.nix
+++ lib/default.nix
@@ -363,7 +363,7 @@ in rec {
                     home.homeDirectory =
                       let
                         username = config.home.username;
-                        homeDir = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
+                        homeDir = if pkgs.stdenv.hostPlatform.isDarwin then "/Users/${username}" else "/home/${username}";
                       in
                       lib.mkDefault homeDir;
                   }
