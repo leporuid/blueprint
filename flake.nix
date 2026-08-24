8fcaf3ad9efa825fb01b00dedbee3bace81a134c
Yu-Min Peng <98006840+leporuid@users.noreply.github.com>
2026-08-24 12:10:38 +0800

    Update systems input URL in flake.nix
    Signed-off-by: Yu-Min Peng <98006840+leporuid@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
+    systems.url = "github:nix-systems/triplet";
   };
 
   outputs =
